280819490d854db95f82670915a2745d078b8a5c
Samuele-Chioda <samuelechioda@gmail.com>
2024-10-31 12:21:21 +0100

    Aggiunto componente SearchCity per la ricerca delle città

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <SearchCity/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SearchCity from './components/SearchCity.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SearchCity
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin:0;
+  padding:0;
+  box-sizing:border-box;
 }
 </style>
---
+++ src/components/SearchCity.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <input type="text" v-model="city" placeholder="inserisci una Città" />
+        <button @click="searchWeather">Cerca Meteo</button>
+    </div>
+</template>
+
+<script>
+export default{
+    name:"SearchCity",
+    data() {
+        return {
+            city:""
+        };
+    },
+    methods: {
+        searchWeather() {
+            console.log(`Cercando il meteo per ${this.city}`);
+        }
+    }
+};
+</script>
+
+<style scoped>
+</style>
